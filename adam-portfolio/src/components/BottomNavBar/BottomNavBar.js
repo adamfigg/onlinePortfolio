a2802e7f2f5ea93d5181e3c833dd117254e911f4
adam f <afiggat@gmail.com>
2018-01-30 20:51:26 -0700

    added a cool effect and made the text show up correctly for the footer. Yay!

--- adam-portfolio/src/components/BottomNavBar/BottomNavBar.js
+++ adam-portfolio/src/components/BottomNavBar/BottomNavBar.js
@@ -7,8 +7,22 @@ class BottomNavBar extends Component {
         return (
 
             <div className="bottomNavBar">
-              
+                <article id="wrap">
                 <p>This is where i will put links to sites and social media stuff</p>
+	<article id="lightings">
+    	<section id="one">
+            <section id="two">
+                <section id="three">
+                    <section id="four">
+                        <section id="five">
+                            
+                        </section>
+                    </section>
+                </section>
+            </section>
+		</section>
+    </article>
+</article>
 
             </div>
 
