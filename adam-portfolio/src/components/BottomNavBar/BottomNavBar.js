49a128828260d6563257df8b300400761aa44c8e
adam f <afiggat@gmail.com>
2018-01-30 20:31:50 -0700

    Fixed a strange error where the bottom navbar would not show up. Only worked once I rewrote the component name, but spell checked and it was just fine.

--- adam-portfolio/src/components/BottomNavBar/BottomNavBar.js
+++ adam-portfolio/src/components/BottomNavBar/BottomNavBar.js
@@ -6,7 +6,7 @@ class BottomNavBar extends Component {
     render() {
         return (
 
-            <div class="bottomNavBar">
+            <div className="bottomNavBar">
                 <p>This is where i will put links to sites and social media stuff</p>
             </div>
 
